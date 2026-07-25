59515b66593da5353eec172332d229948607516d
Tomashu <tomash4my@gmail.com>
2026-07-25 09:08:53 +0200

    update unity to 6000.0.79f1

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -6,7 +6,7 @@ public class Tile
     public string name;
     public TileImage image;
     public TileType type, hidden;
-    public int difficulty, defeatedEnemies, timer;
+    public int difficulty, defeatedEnemies, timer, levels;
     public bool mine, boss, foundTreasure, clear;
 
     public string Name => GetName(type);
